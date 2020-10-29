504ab47b267114a60f45f522bacefd2afa2633ff
Paulina Kasperek <49270882+PaulinaKas@users.noreply.github.com>
2020-10-29 15:44:07 +0100

    Var types for drop_table_if_exists() tested

--- test_main.py
+++ test_main.py
@@ -1,1 +1,26 @@
+import pytest
+import inspect
+
 from main import *
+
+
+class Test_drop_table_if_exists:
+    def test_input_var_types_int(self):
+        input = 123
+        assert drop_table_if_exists(input) == 'DROP TABLE IF EXISTS 123'
+
+    def test_input_var_types_str(self):
+        input = 'table'
+        assert drop_table_if_exists(input) == 'DROP TABLE IF EXISTS table'
+
+    def test_input_var_types_none(self):
+        input = None
+        with pytest.raises(Exception) as excinfo:
+            drop_table_if_exists(input)
+        assert str(excinfo.value) == 'The table name cannot be None object. Please change.'
+
+    def test_input_var_types_sqlite(self):
+        input = 'sqlite_'
+        with pytest.raises(Exception) as excinfo:
+            drop_table_if_exists(input)
+        assert str(excinfo.value) == "The table name cannot starts with 'sqlite_'. Please change."
