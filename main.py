577ec8a123cda6668aaeecbedecbfdeba9fc3cbe
Paulina Kasperek <49270882+PaulinaKas@users.noreply.github.com>
2020-10-30 11:32:00 +0100

    get_table_name() fixed now

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import re
 import pandas as pd # to load column names and their data types
 import random, string # to generate random table name if necessary
 
-csv_file = "student-mat.csv"
+csv_file = "students.csv"
 df_from_csv = pd.read_csv(csv_file, delimiter=';')
 
 
@@ -14,16 +14,18 @@ def get_table_name():
     Create a table name from CSV file name and convert it to be table name
     allowed by slite3 documentation.
     '''
-    if csv_file.startswith('sqlite_'):
-        table_name = ""
+    # when in CSV file name there are letters too
+    regex = re.compile('[^a-z]')
+    table_name = csv_file.split(".")[0]
+    table_name = regex.sub('', table_name)
+
+    # when in CSV file name there aren't any letters
+    if table_name == '':
         for i in range(10):
             table_name += random.choice(string.ascii_lowercase)
-    else:
-        regex = re.compile('[^a-z]')
-        table_name = csv_file.split(".")[0]
-        table_name = regex.sub('', table_name)
-
     return table_name
+
+generated_table_name = get_table_name()
 
 def create_table(df_dataset, table_name):
     '''
@@ -47,10 +49,10 @@ def drop_table_if_exists(table_name):
 
     return f'DROP TABLE IF EXISTS {table_name}'
 
-conn = sqlite3.connect('students.sqlite')
+conn = sqlite3.connect(f'{generated_table_name}.sqlite')
 cur = conn.cursor()
-cur.execute(f"{drop_table_if_exists(get_table_name())}")
-cur.execute(f"{create_table(df_from_csv, get_table_name())}")
+cur.execute(f"{drop_table_if_exists(generated_table_name)}")
+cur.execute(f"{create_table(df_from_csv, generated_table_name)}")
 
 def insert_into_values(df_dataset, table_name):
     '''
@@ -74,9 +76,8 @@ with open(csv_file) as csv_file_with_open:
         for i in variables:
             lis.append(dic[i])
 
-
-        cur.execute(f"{insert_into_values(df_from_csv, get_table_name())}",
+        cur.execute(f"{insert_into_values(df_from_csv, generated_table_name)}",
         tuple(lis))
         conn.commit()
 
-# print(pd.read_sql(f"select * from {get_table_name()}", con = conn))
+# print(pd.read_sql(f"select * from {generated_table_name}", con = conn))
