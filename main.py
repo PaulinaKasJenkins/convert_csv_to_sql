8b13d8175b88e2193263754fee41e34bd27cf01e
Paulina Kasperek <49270882+PaulinaKas@users.noreply.github.com>
2020-10-30 10:19:49 +0100

    Previous created funcs replaced with new one: get_table_name()

--- main.py
+++ main.py
@@ -8,43 +8,12 @@ import random, string # to generate random table name if necessary
 csv_file = "student-mat.csv"
 df_from_csv = pd.read_csv(csv_file, delimiter=';')
 
-def generate_random_table_name():
-    table_name = ""
-    for i in range(30):
-        table_name += random.choice(string.ascii_lowercase)
+
+def get_table_name():
+    regex = re.compile('[^a-zA-Z]')
+    table_name = "student-mat.csv".split(".")[0]
+    table_name = regex.sub('', table_name)
     return table_name
-
-
-def get_result_of_input():
-    reply = input("Please enter a table name (string that doesn't start with '_sqlite' nor a digit): ")
-    first_char = int(reply[0])
-    if isinstance(first_char, int):
-        reply = input("""Please enter a table name (string that doesn't start with
-        '_sqlite' nor a digit). You have last chance, otherwise the table will be named randomly. Please type: """)
-        first_char = int(reply[0])
-        if isinstance(first_char, int):
-            reply = generate_random_table_name()
-    return reply
-
-
-def get_proper_table_name(table_name):
-    '''
-    According to sqlite documentation (https://www.sqlite.org/lang_createtable.html),
-    every string name for table is allowed, in spite of names starting with "slite_".
-    '''
-    if table_name == None or '':
-        raise Exception("The table name cannot be None object or blank. Please change.")
-    elif table_name == 'sqlite_':
-        raise Exception("The table name cannot starts with 'sqlite_'. Please change.")
-    elif isinstance(table_name[0], int):
-        raise Exception("The table name cannot start with a digit. Please change.")
-    elif not isinstance(table_name, str):
-        table_name = str(table_name)
-    else:
-        return table_name
-
-    return table_name
-
 
 def create_table(df_dataset, table_name):
     '''
@@ -70,9 +39,8 @@ def drop_table_if_exists(table_name):
 
 conn = sqlite3.connect('students.sqlite')
 cur = conn.cursor()
-table_name = get_proper_table_name(get_result_of_input())
-cur.execute(f"{drop_table_if_exists(table_name)}")
-cur.execute(f"{create_table(df_from_csv, table_name)}")
+cur.execute(f"{drop_table_if_exists(get_table_name())}")
+cur.execute(f"{create_table(df_from_csv, get_table_name())}")
 
 def insert_into_values(df_dataset, table_name):
     '''
@@ -82,21 +50,23 @@ def insert_into_values(df_dataset, table_name):
     values = str(['?' for i in range(numb_of_columns)]).replace("'", "").replace(']', '').replace('[', '')
     return f'INSERT INTO "{table_name}" VALUES ({values})'
 
-    with open(csv_file) as csv_file_with_open:
-        csv_reader = csv.reader(csv_file_with_open, delimiter=';')
-        columns_number = [i for i in range(len(df_from_csv.columns))]
-        variables = [f'variable{i}' for i in columns_number]
+with open(csv_file) as csv_file_with_open:
+    csv_reader = csv.reader(csv_file_with_open, delimiter=';')
+    columns_number = [i for i in range(len(df_from_csv.columns))]
+    variables = [f'variable{i}' for i in columns_number]
 
-        next(csv_reader) # to skip header
-        for row in csv_reader:
-            dic = dict(zip(variables, row))
+    next(csv_reader) # to skip header
+    for row in csv_reader:
+        dic = dict(zip(variables, row))
 
-            tup = ()
-            lis = list(tup)
-            for i in variables:
-                lis.append(dic[i])
+        tup = ()
+        lis = list(tup)
+        for i in variables:
+            lis.append(dic[i])
 
 
-            cur.execute(f"{insert_into_values(df_from_csv, table_name)}",
-            tuple(lis))
-            conn.commit()
+        cur.execute(f"{insert_into_values(df_from_csv, get_table_name())}",
+        tuple(lis))
+        conn.commit()
+
+# print(pd.read_sql(f"select * from {get_table_name()}", con = conn))
